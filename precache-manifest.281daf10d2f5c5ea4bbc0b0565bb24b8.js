fd15ce0aa3f2603a3ca2fa12149374309d7c1fc6
Rahul Shah <rahulshah7@outlook.com>
2018-12-01 13:55:28 +0000

    Updates

--- precache-manifest.281daf10d2f5c5ea4bbc0b0565bb24b8.js
+++ precache-manifest.281daf10d2f5c5ea4bbc0b0565bb24b8.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/fcc-random-quote-machine/static/js/runtime~main.0b6fa26f.js"
   },
   {
-    "revision": "00697a1ba81913695eb3",
-    "url": "/fcc-random-quote-machine/static/js/main.00697a1b.chunk.js"
+    "revision": "340b1f95e72a0a23130c",
+    "url": "/fcc-random-quote-machine/static/js/main.340b1f95.chunk.js"
   },
   {
     "revision": "3910deadb438cba8f293",
     "url": "/fcc-random-quote-machine/static/js/1.3910dead.chunk.js"
   },
   {
-    "revision": "00697a1ba81913695eb3",
-    "url": "/fcc-random-quote-machine/static/css/main.a7b1f65c.chunk.css"
+    "revision": "340b1f95e72a0a23130c",
+    "url": "/fcc-random-quote-machine/static/css/main.5e10c271.chunk.css"
   },
   {
     "revision": "3910deadb438cba8f293",
     "url": "/fcc-random-quote-machine/static/css/1.3c8e8fa8.chunk.css"
   },
   {
-    "revision": "c7fade2fc36efcc5a05af983c0c17a04",
+    "revision": "ed55984eb22ede3285253a5c43e02e64",
     "url": "/fcc-random-quote-machine/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fcc-random-quote-machine/precache-manifest.1c60b7161421ece2da77d00c531d3c37.js"
+  "/fcc-random-quote-machine/precache-manifest.281daf10d2f5c5ea4bbc0b0565bb24b8.js"
 );
 
 workbox.clientsClaim();
