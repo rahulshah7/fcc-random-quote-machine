4dfb90488688686024e1a7dfa945b5f39501cc2e
Rahul Shah <rahulshah7@outlook.com>
2018-12-05 23:24:54 +0000

    Remove incorrectly applied class

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const QuoteBox = props => {
   return (
-    <div id="quote-box" className="card buttons-bar flow-text">
+    <div id="quote-box" className="card flow-text">
       {props.children}
     </div>
   );
